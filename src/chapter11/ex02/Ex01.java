d63e963ad69c29676c3d784f3e091aa5d12f34d1
Park JW <p.jangwoo@gmail>
2022-11-25 16:09:51 +0900

    11월 25일  - 문제 까지 - 오후 4시 10분

--- src/chapter11/ex02/Ex01.java
+++ src/chapter11/ex02/Ex01.java
@@ -1,19 +1,38 @@
 package chapter11.ex02;
 
-class Studnet {
+class Student {
 	//필드 
-	String nanme; 
+	String name; 
 	
 	//합계와 평균을 출력 하는 메소드
 	void sum_avg () {	
 	}
 }
 
-class Student_Sub extends Studnet{
+class Student_Sub extends Student{
 	//자식의 필드 
 	int kor ;    //국어 점수
 	int eng ; 	 //영어 점수
 	int math;    //수학 점수 
+	
+	
+	public Student_Sub(String name , int kor, int eng, int math) {
+		super();
+		super.name = name; 
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+	
+	@Override
+	void sum_avg() {
+		int sum = kor + eng + math ; 
+		double avg = sum / 3.0 ; 
+		System.out.printf( " %s 학생의 합계는 %d 이고, 평균은 %4.2f 입니다. ", name, sum, avg);
+		System.out.println();
+	}
+	
+	
 	
 	//부모의 sum_avg() 메소드를 오버라이딩해서 객체의 kor , eng, math 의 합계와 평균을 출력 하는 메소드 
 	
@@ -41,6 +60,44 @@ public class Ex01 {
 		    // 5명의 학생의 국어에 대한 합계, 평균
 			// 5명의 학생의 영어에 대한 합계, 평균
 		    // 5명의 학생의 수학에 대한 합계, 평균	
+		
+		System.out.println("1. 객체 생성후 생성자를 사용해서 값 입력==== ");
+		
+		Student s1 = new Student_Sub ("홍길동", 70, 88, 77) ; 
+		Student s2 = new Student_Sub ("이순신", 80, 68, 87) ;
+		Student s3 = new Student_Sub ("김똘똘", 60, 86, 87) ;
+		Student s4 = new Student_Sub ("홍길순", 77, 56, 80) ;
+		Student s5 = new Student_Sub ("강감찬", 55, 66, 77) ;
+		
+		System.out.println("2. 생성된 객체를 Student 배열에 저장====");
+		
+		Student[] arr = new Student[] { s1, s2, s3, s4, s5 } ; 
+		
+		
+		System.out.println("3. 배열의 값을 For 문을 사용해서 출력시 각각의 내용값 출력 "); 
+		
+		int [] sum = new int[3];    //sum[0] : 국어 합계 , sum[1] : 영어 합계, sum[2] : 수학 합계
+		// 각 학생의 합계 및 평균
+		for (int i = 0 ; i < arr.length ; i ++) {
+			// 각 학생의 합계및 평균을 오버라이딩된 메소드로 호출
+			arr[i].sum_avg() ;
+			
+			sum[0] += ((Student_Sub) arr[i]).kor;
+			sum[1] += ((Student_Sub) arr[i]).eng;
+			sum[2] += ((Student_Sub) arr[i]).math;
+			
+			if ( i == arr.length-1) {
+				System.out.println("=======================");
+				System.out.printf("5명의 국어 점수의 총 합은 %d 이고 평균은 %4.2f 입니다  ", sum[0], sum[0]/5.0);
+				System.out.println();
+				System.out.printf("5명의 영어 점수의 총 합은 %d 이고 평균은 %4.2f 입니다  ", sum[1], sum[1]/5.0);
+				System.out.println();
+				System.out.printf("5명의 수학 점수의 총 합은 %d 이고 평균은 %4.2f 입니다  ", sum[2], sum[2]/5.0);
+				System.out.println();
+			}
+		}
+		
+		
 	}
 
 }
